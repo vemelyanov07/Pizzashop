06bfb25eb2ec1aaa50c93eb45b4680cc8798cff6
IgorKrivchik <k13stels@inbox.ru>
2022-07-25 00:36:51 +0300

    add release_notes

--- pizzashop/address/views.py
+++ pizzashop/address/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from .forms import AddNewaddress
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = AddNewaddress(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    else:
+        form = AddNewaddress()
+
+    return render(request, 'address.html', {'form': form})
