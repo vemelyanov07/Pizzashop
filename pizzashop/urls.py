e91ad77d6bc0789a88aebaa079020f53e705304c
Kiryl Lapin <lapin.kirill45@gmail.com>
2022-07-27 21:03:00 +0200

    fix: login

--- pizzashop/urls.py
+++ pizzashop/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('address/', include('address.urls')),
     path('cart/', include('cart.urls')),
-    path('main/', include('main.urls')),
+    path('', include('main.urls')),
     path('menu/', include('menu.urls')),
 ]
