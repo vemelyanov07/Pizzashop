db1bdc592066a00629a4174d94d4afe4efdd1dd7
Kiryl Lapin <lapin.kirill45@gmail.com>
2022-09-04 12:12:24 +0200

    - Add postgre settings

--- pizzashop/settings.py
+++ pizzashop/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'pizzashop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'da0ph6rcefp02v',
+        'USER': 'adfirdvohmhbzx',
+        'PASSWORD': '734462e4e62c323fb6017aff3ae81c48fdeb23d688a1927cb8da3d09d33f874b',
+        'HOST': 'ec2-3-223-242-224.compute-1.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
@@ -124,10 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
